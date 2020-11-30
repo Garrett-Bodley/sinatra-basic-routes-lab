6617d81c9c6d77e2bc0deb291173f3f8946b48b4
Garrett <Garrett.Bodley@gmail.com>
2020-11-30 17:03:42 -0500

    commit tree 376a9c83254ad4d48b5b9bd01e989548dc1535a4

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Garrett."
+  end
+
+  get '/hometown' do
+    "My hometown is Kailua."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Reflections by Thelonious Monk."
+  end
+
 end
